b5111d54eef02046b634c38fbc2ab15da8535023
RuWander <ruandn@gmail.com>
2019-10-03 17:48:37 +0200

    fixed styling issues on event display page

--- src/app/results-blocks/results-blocks.component.ts
+++ src/app/results-blocks/results-blocks.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../core/data.service';
 import { Event } from '../core/data-types';
-import { group } from '@angular/animations';
 
 @Component({
   selector: 'app-results-blocks',
@@ -10,19 +9,26 @@ import { group } from '@angular/animations';
 })
 export class ResultsBlocksComponent implements OnInit {
   private eventsData: Event[] = [];
+  // should be worked into data in database
+  private groupTotals = [];
 
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
     this.dataService.getEvents().subscribe(events => {
-      // console.log(events);
       this.eventsData = events;
       events.map(event => {
-        event.groups.map((group, i) => {
-          const newCrits = [...group.criteria];
-          console.log(typeof newCrits);
+        event.groups.map(group => {
+          const newGroup = { id: group.id, totalValue: 0 };
+          console.log(group.criteria);
+          group.criteria.map(crit => {
+            newGroup.totalValue += crit.value;
+          });
+          newGroup.totalValue = newGroup.totalValue / group.criteria.length;
+          this.groupTotals.push(newGroup);
         });
       });
+      console.log(this.groupTotals);
     });
   }
 }
